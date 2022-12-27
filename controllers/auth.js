8696b24d0cd90bf919b8a9597be26b90ee5b7f48
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-12-27 18:18:52 -0500

    Configuración de cors y body de las peticiones

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,10 @@
 const { response } = require('express');
 
 const crearUsuario = (req, res = response) => {
+
+  const { name, email, password } = req.body;
+  console.log(name, email, password);
+  
   return res.json({
     ok: true,
     msg: 'Crear usuario /new'
@@ -8,6 +12,10 @@ const crearUsuario = (req, res = response) => {
 }
 
 const loginUsuario = (req, res) => {
+
+  const { email, password } = req.body;
+  console.log( email, password);
+  
   return res.json({
     ok: true,
     msg: 'Login de usuario /'
--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
-
+const cors = require('cors');
 
 // Create express server/app 
 const app = express();
+
+// cors
+app.use( cors() );
+
+// lectura y parseo del body
+app.use( express.json() );
 
 // rutas
 app.use('/api/auth', require('./routes/auth') );
