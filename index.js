279747acbca41661b018560a8f2944ec0592beed
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-12-27 17:09:52 -0500

    Configuracion del servidor express

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hello from Node')
+const express = require('express');
+
+
+// Create express server/app 
+const app = express();
+
+// GET
+app.get('/', (req, res) => {
+  res.json({
+    ok: true,
+    message: 'Todo salio bien',
+    uid: 1234
+  })
+} );
+
+app.listen( 4000, () => {
+  console.log(`Server is running on port ${4000}`);
+} );
