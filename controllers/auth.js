6dbfd96735b1f332c8cc0acfafbd9864954b89d7
Fabián Montoya <jose.fabian.montoya9@gmail.com>
2022-12-27 21:12:12 -0500

    Guardar usuarios en la db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,54 @@
 const { response } = require('express');
+const Usuario = require('../models/Usuario');
 
-const crearUsuario = (req, res = response) => {
+const crearUsuario = async (req, res = response) => {
 
   const { name, email, password } = req.body;
-  
-  return res.json({
-    ok: true,
-    msg: 'Crear usuario /new'
-  });
+
+  try {
+
+    // Verificar si el email existe
+    let usuario = await Usuario.findOne({ email });
+
+    if (usuario) {
+      return res.status(400).json({
+        ok: false,
+        msg: 'Ya existe un usuario con ese email'
+      })
+    }
+
+    // Crear usuario con el modelo
+    const dbUser = new Usuario(req.body);
+
+    // Hashear el password
+
+    // Generar el JsonWebToken
+
+    // Guardar usuario en db
+    await dbUser.save();
+
+    // Generar respuesta exitosa
+    return res.status(201).json({
+      ok: true,
+      uid: dbUser.id,
+      name
+    })
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      ok: false,
+      msg: 'Hable con el administrador'
+    });
+  }
+
+
+
 }
 
 const loginUsuario = (req, res = response) => {
 
-  const { email, password } = req.body;  
+  const { email, password } = req.body;
 
   return res.json({
     ok: true,
